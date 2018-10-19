d000ff27e32b9975be99a321dc7c1732048b5129
Blueberry00 <2802843862@qq.com>
2018-10-19 23:23:11 +0800

    “详情页的跳转 git commit -m“详情页的跳转

--- index.js
+++ index.js
@@ -31,8 +31,6 @@ app.get('/', function (req, res) {
     from okr`, function (err, data) {
             var phone = req.cookies.username;
 
-            // console.log('data:', data)
- 
             res.render('homePage.html', { phone: phone, okrs: data, title: '首页' });
         })
 });
@@ -40,52 +38,49 @@ app.get('/', function (req, res) {
 app.get('/details/:id', function (req, res) {
 
     var id = req.params.id;
-   connection.query(`select *,
+    connection.query(`select *,
    (select phone from user where user.id = okr.user_id) as phone
-   from okr where id=? limit 1`,[id], function(err,data) {
-    // console.log('okr data: ' , data)
-
-    res.render('details.html',{okr: data[0] });
-   })
- 
+   from okr where id=? limit 1`, [id], function (err, data) {
+          var phone = req.cookies.username;
+            res.render('details.html', { phone: phone, okr: data[0] })
+        })
 });
 
+// app.get('/details', function (req, res) {
+//     var id = req.params.id;
 
-app.get('/details', function (req, res) {
-    connection.query('select * from okr', function (err, data) {
-        var username = req.cookies.username;
-        
-        res.render('details.html', {username: username, okrs : data, title:'详情'});
-    })
-});
+// connection.query('select * from comment', function (err, data) {
+//     var datas = this.data;
+//     res.render('details.html', {commen : data});
+// }
+// });
 
 
 app.post('/api/comments', function (req, res) {
-
-    var oke_id = id;
+    // var oke_id = id;
     var uid = req.cookies.uid;
     var writeIn = req.body.write;
     var created_at = moment().format('YYYY-MM-DD HH:MM:SS');
 
-    connection.query('insert into comment values (null,? , ? , ? , ?)',[ oke_id,uid, writeIn, created_at] , function (err, data) {
-        console.log('data:', data)
+    connection.query('insert into comment values (null,"" , ? , ? , ?)', [uid, writeIn, created_at], function (err, data) {
+        // console.log('data:', data)
         res.send("评论成功");
     })
 });
 
 app.post('/api/homePage', function (req, res) {
-    
     var phone = req.body.phone;
     var password = req.body.password;
+    var username = phone.substr(0, 3) + "****" + phone.substr(7);
     var created_at = moment().format('YYYY-MM-DD HH:MM:SS');
 
-    connection.query('insert into user values (null , ? , ? , "" , "" , "" , ?)', [phone, password, created_at], function (err, data) {
-        //    console.log('data:' ,data)
+    connection.query('insert into user values (null , ? , ? , ? , "" , "" , ?)', [phone, password, username, created_at], function (err, data) {
+        // console.log('data:', data)
         res.send("注册成功");
     });
 });
 
-app.post('/api/articles',function(req,res){
+app.post('/api/articles', function (req, res) {
     var title = req.body.object;
     var actions = req.body.action;
     var user_name = req.cookies.uid;
@@ -93,30 +88,34 @@ app.post('/api/articles',function(req,res){
 
     var created_at = moment().format('YYYY-MM-DD HH:mm:ss');
     // console.log(title,actions,created_at);
-    
-    connection.query('insert into okr values (null,?,"",?, ? ,?)', [title, actions ,user_name ,created_at], function (err, data) {
+
+    connection.query('insert into okr values (null,?,"",?, ? ,?)', [title, actions, user_name, created_at], function (err, data) {
         res.send("发布成功");
     });
 });
 
+
 app.post('/api/login', function (req, res) {
     var phone = req.body.phone;
     var password = req.body.password;
-
     connection.query('select * from user where phone=? and password=? limit 1', [phone, password], function (err, data) {
         if (data.length > 0) {
             res.cookie('uid', data[0].id);
-            res.cookie('user_name',data[0].username);
-            res.cookie('username', data[0].phone);
-            res.cookie('time',data[0].created_at);
-
-            res.send('登陆成功');
-            // res.render('homePage.html');
+                res.cookie('user_name', data[0].username);
+                res.cookie('username', data[0].phone);
+                res.cookie('time', data[0].created_at);
+            var token = phone + password + new Date().getTime() + Math.random();
+            connection.query('update user as t set t.token = ? where  phone=? ', [token, phone], function (err, data) {
+                res.cookie('token', token)
+               
+                res.send('登陆成功');
+            });
         } else {
             res.send('对不起，用户名或密码错误')
         }
     })
 })
+
 
 
 // app.get('/details', function (req, res) {
