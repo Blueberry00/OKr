c7d60b0e01545e7a388bb2333c35dda87186c90c
Blueberry00 <2802843862@qq.com>
2018-11-03 19:47:16 +0800

    ajax实现登陆注册评论

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ nunjucks.configure('views', {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+//个人首页拉取
 app.get('/api/homepage', function (req, res) {
     res.cookie("test", "value");
     connection.query(`select *,
@@ -31,15 +32,11 @@ app.get('/api/homepage', function (req, res) {
     from okr`, function (err, data) {
             var phone = req.cookies.username;
             //   console.log('phone: ',phone)
-            res.json({ data , phone:phone});
-            // console.log(data)
+            res.json({ data, phone: phone });
         })
 });
 
-app.get('/', function (req, res) {
-    res.render('homePage.html')
-})
-
+//详情页okr的获取
 app.get('/api/details/:id', function (req, res) {
     var id = req.params.id;
 
@@ -53,20 +50,7 @@ app.get('/api/details/:id', function (req, res) {
         })
 })
 
-// app.get('/api/comments/:okr_id', function (req, res) {
-//     var okr_id = req.query.okr_id;
-//     var page = req.query.page || 1;
-//     var size = 10;
-
-//     connection.query(`select *,
-// (select username from user where user.id=comment.user_id) as username,
-// (select avatar from user where user.id=comment.user_id) as avatar
-//                     from comment where okr_id=? limit ?, ?`, [okr_id, (page - 1) * size, size], function (err, data) {
-//             res.json(data);
-//             console.log('data: ', data)
-//         })
-// });
-
+//okr详情页面评论
 app.get('/api/comments/:id', function (req, res) {
     var okr_id = req.params.id;
 
@@ -78,62 +62,41 @@ app.get('/api/comments/:id', function (req, res) {
         })
 });
 
+// 个人页面
+app.get('/api/personal/:id', function (req, res) {
+    var id = req.params.id;
 
+    connection.query(`select * ,
+    (select object from okr where okr.user_id = user.id) as object
+    from user where id=?`, [id], function (err, data) {
+            // var phone = req.cookies.username;
+            res.json({ data })
+            console.log(data)
+        })
+});
+
+//api所有页面
+app.get('/api/*', function (req, res) {
+    var url = req.originalUrl;
+    request.get('localhost:3000' + url, function (err, resp) {
+        res.send(JSON.parse(resp.body))
+    })
+})
+//详情页面
 app.get('/details', function (req, res) {
 
     res.render('details.html')
 })
 
-// app.get('/details/:id', function (req, res) {
-//     var id = req.params.id;
-
-//     connection.query(`select *,
-//    (select phone from user where user.id = okr.user_id) as phone
-//    from okr where id=? limit 1`, [id], function (err, data) {
-//             res.cookie('okr_id', id);
-//             var phone = req.cookies.username;
-//             res.render('details.html', { phone: phone, okr: data[0], id: id })
-//         })
-// });
-
-
-// ????
-app.get('/api/presonal/:id', function (req, res) {
-    var user_id = req.params.id;
-
-    connection.query(`select *,
-    (select phone from user where user.id = okr.user_id) as phone
-    from okr where user_id=?`, [user_id], function (err, data) {
-            var phone = req.cookies.username;
-            res.json(data)
-            console.log(data)
-        })
-});
-
-
-// app.get('/detailsokr/:id',function (req ,res){
-//     var id = req.params.id;
-//     connection.query(`select * from okr where id=?`,[id],function (err,data){
-//         console.log('data:',data)
-//         res.json(data[0]);
-//     })
-//     // res.render('detailsokr.html');
-// })
-
-
-
-app.get('/api/*', function (req, res){
-    var url = req.originalUrl;
-    request.get('localhost:3000' + url, function(err,resp){
-        res.send(JSON.parse(resp.body))
-    })
-})
-
+//个人页面
 app.get('/center', function (req, res) {
     res.render('PersonalCenter.html')
 });
 
-
+//个人首页
+app.get('/', function (req, res) {
+    res.render('homePage.html')
+})
 
 //评论
 app.post('/api/comments', function (req, res) {
@@ -147,8 +110,8 @@ app.post('/api/comments', function (req, res) {
         res.send("评论成功");
     })
 });
-//注册
 
+//注册
 app.post('/api/homePage', function (req, res) {
     var phone = req.body.phone;
     var password = req.body.password;
@@ -160,17 +123,15 @@ app.post('/api/homePage', function (req, res) {
         // console.log('data :',data)
         if (data.length > 0) {
             res.send('用户名已存在');
-        }else{
+        } else {
             connection.query('insert into user values (null, ?, ?, ?, "", ?, ?)', [phone, password, username, token, created_at], function (err, data) {
-                res.json({data});
+                res.json({ data });
             });
         }
     })
 });
 
-
 //发布
-
 app.post('/api/articles', function (req, res) {
     var title = req.body.object;
     var key_results = req.body.key_results;
@@ -188,7 +149,7 @@ app.post('/api/login', function (req, res) {
     var phone = req.body.phone;
     var password = req.body.password;
     connection.query('select * from user where phone=? and password=? limit 1', [phone, password], function (err, data) {
-        console.log('data:',data)
+        console.log('data:', data)
         if (data.length > 0) {
             res.cookie('uid', data[0].id);
             res.cookie('user_name', data[0].username);
@@ -198,23 +159,12 @@ app.post('/api/login', function (req, res) {
             var token = phone + password + new Date().getTime() + Math.random();
             connection.query('update user as t set t.token = ? where  phone=? ', [token, phone], function (err, data) {
                 res.cookie('token', token)
-                res.json({data});
+                res.json({ data });
             });
-        }else{
+        } else {
             res.send('对不起');
-        } 
+        }
     })
 })
-
-
-
-
-// app.get('/details', function (req, res) {
-//     res.render('details.html')
-// });
-
-
-
-
 
 app.listen(3000);
