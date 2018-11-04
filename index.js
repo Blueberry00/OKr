0818203c457a5ef161c0f02c5b7133942fcf15cc
Blueberry00 <2802843862@qq.com>
2018-11-04 22:00:22 +0800

    ajax实现登陆注册评论,

--- index.js
+++ index.js
@@ -24,17 +24,24 @@ nunjucks.configure('views', {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//个人首页拉取
+//首页拉取
 app.get('/api/homepage', function (req, res) {
     res.cookie("test", "value");
     connection.query(`select *,
     (select phone from user where user.id = okr.user_id) as phone
     from okr`, function (err, data) {
             var phone = req.cookies.username;
-            //   console.log('phone: ',phone)
-            res.json({ data, phone: phone });
+            res.json({data});
+            // console.log('data: ',data)
         })
 });
+
+//????各页面登陆ID
+app.get('/api/phones',function(req,res){
+    var phone = req.cookies.username;
+    
+    res.json({phone})
+})
 
 //详情页okr的获取
 app.get('/api/details/:id', function (req, res) {
@@ -45,7 +52,7 @@ app.get('/api/details/:id', function (req, res) {
    from okr where id=? limit 1`, [id], function (err, data) {
             res.cookie('okr_id', id);
             var phone = req.cookies.username;
-            res.json({ data });
+            res.json({ data,phone:phone});
             // console.log('data: ',data)
         })
 })
@@ -62,18 +69,17 @@ app.get('/api/comments/:id', function (req, res) {
         })
 });
 
-// 个人页面
-app.get('/api/personal/:id', function (req, res) {
-    var id = req.params.id;
+//个人页面的okr
+app.get('/api/userOkr/:id', function (req, res) {
+    var user_id = req.params.id;
 
     connection.query(`select * ,
-    (select object from okr where okr.user_id = user.id) as object
-    from user where id=?`, [id], function (err, data) {
-            // var phone = req.cookies.username;
-            res.json({ data })
-            console.log(data)
+    (select username from user where user.id = okr.user_id) as username
+    from okr where user_id=?`,[user_id], function (err, data) {
+            res.json({data, user_id : user_id});
+            // console.log('data: ', data);
         })
-});
+})
 
 //api所有页面
 app.get('/api/*', function (req, res) {
@@ -82,10 +88,16 @@ app.get('/api/*', function (req, res) {
         res.send(JSON.parse(resp.body))
     })
 })
+
 //详情页面
 app.get('/details', function (req, res) {
 
     res.render('details.html')
+})
+
+//加载的零时页面
+app.get('/d',function(req,res){
+    res.render('jiazai.html')
 })
 
 //个人页面
